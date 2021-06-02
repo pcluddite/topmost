76001e6baf8f36943e610b3ff9f7ec824b3bf7e6
Tim Baxendale <pcluddite@outlook.com>
2021-06-01 19:01:56 -0500

    Add User32Exception

---
+++ Topmost/Interop/User32Exception.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Topmost.Interop
+{
+    internal class User32Exception : Exception
+    {
+        public int LastError { get; private set;  }
+
+        public User32Exception()
+            : this(Marshal.GetLastWin32Error())
+        {
+        }
+
+        public User32Exception(int lastError)
+            : base("An error occurred in user32.dll (error code " + lastError + ")")
+        {
+        }
+
+        public User32Exception(string message)
+            : base(message)
+        {
+            LastError = Marshal.GetLastWin32Error();
+        }
+
+        public User32Exception(string message, int lastError)
+            : base(message)
+        {
+            LastError = lastError;
+        }
+    }
+}
